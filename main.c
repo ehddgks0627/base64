e1a36e1fca26db05dcd125e4d123383be41478df
ehdgks0627 <ehdgks0627@naver.com>
2017-01-14 20:51:01 +0900

    fix table

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-char table[64] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
+char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 void Encoding();
 void Decoding();
